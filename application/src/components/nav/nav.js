082bcee246388c09baad5f230e6d384587600531
Soleras3 <soleras3games@gmail.com>
2023-03-18 05:29:28 -0500

    feat(user display): add email to log out label in nav bar

--- application/src/components/nav/nav.js
+++ application/src/components/nav/nav.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from 'react-redux';
 import "./nav.css";
 
 const Nav = (props) => {
+    const auth = useSelector((state) => state.auth);
+
     return (
         <div className="nav-strip">
             <Link to={"/order"} className="nav-link">
@@ -17,7 +20,7 @@ const Nav = (props) => {
             </Link>
             <Link to={"/login"} className="nav-link">
                 <div className="nav-link-style">
-                    <label className="nav-label">Log Out</label>
+                    <label className="nav-label">Log Out ({auth.email})</label>
                 </div>
             </Link>
         </div>
